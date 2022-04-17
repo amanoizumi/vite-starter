9043e9344de19751f235f3ab61db23c7b649fab6
amanoizumi <a0918233246@gmail.com>
2022-04-17 09:49:36 +0800

    update: package.json 下載 @iconify/json ，順利呈現畫面上的 icon

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <Layout>
    index content
+    <heroIconsOutlineLocation/>
   </Layout>
 </template>
 <script>
+import heroIconsOutlineLocation from '~icons/heroicons-outline/location-marker';
 
 export default {
-
+  components: {
+    heroIconsOutlineLocation,
+  },
+  setup() {
+    return {
+      
+    }
+  }
 };
 </script>
